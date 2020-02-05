1dae067e6355a96f12ec9c7138f7eda8aa66c74e
horup <sah@sah.dk>
2020-02-05 07:06:23 +0100

    updated controller name

---
+++ Backend/Controllers/MeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class MeController : ControllerBase
+    {
+        private readonly ILogger<MeController> _logger;
+
+        public MeController(ILogger<MeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<object> Get()
+        {
+            var usr = User;
+            var claims = usr.Claims.Select((c,i)=>
+            {
+                return new {Type = c.Type, Value = c.Value, 
+                ValueType = c.ValueType, Issuer = c.Issuer};
+            });
+            return claims;
+        }
+    }
+}
